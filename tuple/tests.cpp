2dc768f88d61c38bfadf23595c97f38239bb5a0f
pew-pew <pew-pew@users.noreply.github.com>
2018-05-12 12:37:00 +0300

    Move and comparison tests

--- tuple/tests.cpp
+++ tuple/tests.cpp
@@ -7,6 +7,20 @@ constexpr bool is_same = std::is_same<T, U>::value;
 
 #define ASSERT_IS(expr, type) static_assert(is_same<decltype((expr)), type>, "")
 
+typedef Tuple<int, float, int> Tuple_int_float_int;
+TEST(Tuple, Constructor) {
+    Tuple_int_float_int t;
+    EXPECT_EQ(t, Tuple_int_float_int(0, 0.0f, 0));
+}
+
+TEST(Tuple, Comparison) {
+    EXPECT_EQ(Tuple_int_float_int(1, 3.14f, 2), Tuple_int_float_int(1, 3.14f, 2));
+    EXPECT_NE(Tuple_int_float_int(1, 3.15f, 2), Tuple_int_float_int(1, 3.14f, 2));
+    EXPECT_LT(Tuple_int_float_int(0, 3.14f, 2), Tuple_int_float_int(1, 3.14f, 2));
+    EXPECT_LT(Tuple_int_float_int(1, 3.13f, 2), Tuple_int_float_int(1, 3.14f, 2));
+    EXPECT_GT(Tuple_int_float_int(2, 3.13f, 2), Tuple_int_float_int(1, 3.14f, 2));
+}
+
 TEST(Tuple, Get) {
     Tuple<int, float, int> t(1, 3.14f, 3);
     const Tuple<int, float, int> &const_t = t;
@@ -49,6 +63,21 @@ TEST(Tuple, Concatenate) {
     auto x = tupleCat(Tuple<int>(5), Tuple<>(), Tuple<float>(7), Tuple<char>('#'));
     ASSERT_IS(x, Tuple_ifc&);
     EXPECT_EQ(x, Tuple_ifc(5, 7, '#'));
+}
+
+struct NoCopy {
+    NoCopy() = default;
+    NoCopy(const NoCopy&) = delete;
+    NoCopy(NoCopy&&) = default;
+
+    NoCopy& operator=(const NoCopy&) = delete;
+    NoCopy& operator=(NoCopy&&) = default;
+};
+
+TEST(Tuple, Move) {
+    NoCopy x;
+    (void)(Tuple<NoCopy, NoCopy>(std::move(x), NoCopy()));
+    (void)(tupleCat(Tuple<NoCopy>(), Tuple<NoCopy>()));
 }
 
 int main(int argc, char** argv) {
